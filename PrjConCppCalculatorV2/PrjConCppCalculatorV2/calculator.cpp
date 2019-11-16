964dab5ee822d8ba0b2684af2e21d0aa463b1fa0
AndresArdila89 <andresardila@me.com>
2019-11-15 19:43:13 -0500

    iomanip library added

--- PrjConCppCalculatorV2/PrjConCppCalculatorV2/calculator.cpp
+++ PrjConCppCalculatorV2/PrjConCppCalculatorV2/calculator.cpp
@@ -1,7 +1,13 @@
+#include <iomanip>
 #include "LaSalleLibraryOne.h"
+
 
 int main() {
 
+	
+	DisplayTitle("calculator");
+	cout << setfill(' ') << setw(15); 
+	cout<<"hello again\n";
 	system("pause");
 	return 0;
 }
